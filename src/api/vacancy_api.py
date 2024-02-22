509f944f27948030f1a635fc62d973b017022b5a
petrovi4 <40.10.444@gmail.com>
2024-02-22 10:36:41 +0300

    add ABC class VacancyAPI

---
+++ src/api/vacancy_api.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class VacancyAPI(ABC):
+    """
+    Абстрактный класс, для работы с API сайтов с вакансиями.
+    """
+
+    def __init__(self, url, count_vacancies=100):
+        self._url = url
+        self._count_vacancies = count_vacancies
+
+    @abstractmethod
+    def get_vacancies(self, vacancy_title):
+        pass
